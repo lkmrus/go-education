58f02dfbe0b1b40bc5de947381e6443af5f5fd68
Ruslan Lukmanov <lkmrus@gmail.com>
2024-11-12 21:24:31 -0300

    env

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+)
 
 func loopback() func() {
 	i := 10
@@ -11,10 +15,15 @@ func loopback() func() {
 }
 
 func main() {
-	loop := loopback()
-	loop()
-	loop()
-	loop()
-	loop()
-	loop()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	TEST := os.Getenv("TEST")
+	envArray := os.Environ()
+	for _, env := range envArray {
+		fmt.Println(env)
+	}
+	fmt.Println(TEST)
+
 }
