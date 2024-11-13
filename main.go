fbff064117caed88c69d52a1b2f58ba4c534f6f5
Ruslan Lukmanov <lkmrus@gmail.com>
2024-11-12 21:12:20 -0300

    import and class work

--- main.go
+++ main.go
@@ -2,58 +2,19 @@ package main
 
 import "fmt"
 
-func add(a, b float64) float64 {
-	return a + b
-}
-
-func sub(a, b float64) float64 {
-	return a - b
-}
-
-func mul(a, b float64) float64 {
-	return a * b
-}
-
-func div(a, b float64) float64 {
-	return a / b
-}
-
-func execMath(function MathFunc, a, b float64) {
-	mathFunc := function
-	if mathFunc == nil {
-		fmt.Println("Function not found")
-		return
+func loopback() func() {
+	i := 10
+	return func() {
+		i += 1
+		fmt.Println(i)
 	}
-
-	result := mathFunc(a, b)
-	fmt.Println("Result: ", result)
-}
-
-type MathFunc func(a, b float64) float64
-type Math map[string]MathFunc
-
-var mathMap = Math{
-	"add": add,
-	"sub": sub,
-	"mul": mul,
-	"div": div,
 }
 
 func main() {
-	var funcName string
-	fmt.Println("Enter function name: ")
-	fmt.Scan(&funcName)
-
-	var math MathFunc = mathMap[funcName]
-	if math == nil {
-		fmt.Println("Function not found")
-		return
-	}
-
-	fmt.Println("Enter function args: ")
-	var a, b float64
-	fmt.Scan(&a, &b)
-	result := math(a, b)
-	fmt.Println("Result: ", result)
-	execMath(mathMap["add"], 1, 2)
+	loop := loopback()
+	loop()
+	loop()
+	loop()
+	loop()
+	loop()
 }
