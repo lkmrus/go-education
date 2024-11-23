1d32d7a4b44c146ad4d4bd1bd54b42683d5ead3c
Ruslan Lukmanov <lkmrus@gmail.com>
2024-11-23 20:24:44 -0300

    init server

--- internal/auth/user.go
+++ internal/auth/user.go
@@ -1,7 +1,9 @@
 package auth
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
@@ -12,8 +14,8 @@ func Route() *mux.Router {
 
 	r := mux.NewRouter()
 	router := r.PathPrefix("/user").Subrouter()
-	router.HandleFunc("/register", user.register)
-	router.HandleFunc("/login", user.login)
+	router.HandleFunc("/register", user.register).Methods("POST")
+	router.HandleFunc("/login", user.login).Methods("POST")
 	return router
 }
 
@@ -22,5 +24,32 @@ func (u *User) register(writer http.ResponseWriter, request *http.Request) {
 }
 
 func (u *User) login(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte("Login!"))
+	var payload LoginRequest
+	response := LoginResponse{
+		Token: "token",
+	}
+
+	err := json.NewDecoder(request.Body).Decode(&payload)
+	if err != nil {
+		Json(writer, err.Error(), 402)
+		return
+	}
+
+	if payload.Email == "" || payload.Password == "" {
+		Json(writer, "Email and password are required", 400)
+		return
+	}
+
+	Json(writer, response, 201)
+}
+
+func Json(writer http.ResponseWriter, response any, statusCode int) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(statusCode)
+
+	jsonBytes, err := json.Marshal(response)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	writer.Write(jsonBytes)
 }
